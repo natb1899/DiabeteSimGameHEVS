5a685a87d9b697aed0fe0d56a7d0c2acfe258375
natb1899 <natal.bumann@students.hevs.ch>
2023-04-03 09:53:15 +0200

    change the tiles sets

--- assets/tiles/tiles_sets/cantine.tsx
+++ assets/tiles/tiles_sets/cantine.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="cantine" tilewidth="16" tileheight="16" tilecount="2565" columns="57">
- <image source="../../images/sprite/cantine.png" width="918" height="720"/>
+ <image source="sprite/cantine.png" width="918" height="720"/>
 </tileset>
