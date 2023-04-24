d93213bf88f1e01fe910dcc9027d3294697466f7
DnPint <daniel.pintodem@hes-so.ch>
2023-04-24 15:04:38 +0200

    added door to nurse office

--- assets/tiles/tiles_sets/hopital.tsx
+++ assets/tiles/tiles_sets/hopital.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="hopital" tilewidth="16" tileheight="16" tilecount="2565" columns="57">
- <image source="sprite/hopital.png" width="918" height="720"/>
+ <image source="../../images/sprite/hopital.png" width="918" height="720"/>
 </tileset>
--- assets/tiles/tiles_sets/joueur.tsx
+++ assets/tiles/tiles_sets/joueur.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="joueur" tilewidth="16" tileheight="16" tilecount="3420" columns="57">
- <image source="sprite/joueur.png" width="918" height="969"/>
+ <image source="../../images/sprite/joueur.png" width="918" height="969"/>
 </tileset>
