3e6e1ed95db77869af06c05eb9104248c7c19c71
natb1899 <natal.bumann@students.hevs.ch>
2023-04-25 09:26:12 +0200

    changes tiles_sets

--- assets/tiles/tiles_sets/ami positif.tsx
+++ assets/tiles/tiles_sets/ami positif.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="ami positif" tilewidth="16" tileheight="16" tilecount="3420" columns="57">
- <image source="../../images/sprite/amipositif.png" width="918" height="969"/>
+ <image source="sprite/amipositif.png" width="918" height="969"/>
 </tileset>
